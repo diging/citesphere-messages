07419cc0b252c6ff5f948fd83fcae14e2b059c24
Julia Damerow <julia.damerow@gmail.com>
2020-01-30 16:44:44 -0500

    [CITE-79] added kafka topic for export

--- citesphere-messages/src/main/java/edu/asu/diging/citesphere/messages/KafkaTopics.java
+++ citesphere-messages/src/main/java/edu/asu/diging/citesphere/messages/KafkaTopics.java
@@ -4,4 +4,6 @@ public interface KafkaTopics {
 
     public final static String REFERENCES_IMPORT_TOPIC = "_citesphere_import_references_topic";
     public final static String REFERENCES_IMPORT_DONE_TOPIC = "_citesphere_import_references_done_topic";
+
+    public final static String REFERENCES_EXPORT_TOPIC = "_citesphere_export_references_topic";
 }
