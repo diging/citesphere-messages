bb4936035e55da06073f23089fd36a3d22e74c34
Pratik Prakash Giri <32401726+PratikGiri@users.noreply.github.com>
2023-03-07 17:08:38 -0700

    [CITE-177] Adding the Kafka topic for Crossref

--- citesphere-messages/src/main/java/edu/asu/diging/citesphere/messages/KafkaTopics.java
+++ citesphere-messages/src/main/java/edu/asu/diging/citesphere/messages/KafkaTopics.java
@@ -7,4 +7,5 @@ public interface KafkaTopics {
 
     public final static String REFERENCES_EXPORT_TOPIC = "_citesphere_export_references_topic";
     public final static String REFERENCES_EXPORT_DONE_TOPIC = "_citesphere_export_references_done_topic";
+    public final static String REFERENCES_IMPORT_CROSSREF_TOPIC = "_citesphere_import_crossref_topic";
 }
